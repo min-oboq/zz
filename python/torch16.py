42f033223dced09626c3ffe2a59f5a6ec578e117
min-oboq <mj4870@naver.com>
2023-09-07 16:37:03 +0900

    0907

---
+++ python/torch16.py
@@ -0,0 +1,99 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+
+print(torch.cuda.is_available())
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+train_dataset = torchvision.datasets.FashionMNIST('pytorch/data', download=True, transform=transforms.Compose([transforms.ToTensor()]))
+test_dataset = torchvision.datasets.FashionMNIST('pytorch/data', download=True, train=False, transform=transforms.Compose([transforms.ToTensor()]))
+train_loader = DataLoader(train_dataset, batch_size=100)
+test_loader = DataLoader(test_dataset, batch_size=100)
+
+class FashionCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 32, 3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, 3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.fc1 = nn.Linear(64*6*6, 600) # 784
+        self.drop = nn.Dropout2d(0.25)
+        self.fc2 = nn.Linear(600, 120)
+        self.fc3 = nn.Linear(120, 10)
+    
+    def forward(self, input_data):
+        out = self.layer1(input_data)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = self.drop(out)
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
+
+learning_rate = 0.001
+model = FashionCNN()
+print(model)
+model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+num_epochs = 5
+count = 0
+loss_list = []
+iteration_list = []
+accuracy_list = []
+predictions_list = []
+labels_list = []
+
+for epoch in range(num_epochs):
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        train = Variable(images.view(100, 1, 28, 28))
+        labels = Variable(labels)
+
+        outputs = model(train)
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        count += 1
+
+        if not (count % 50):
+            total = 0
+            correct = 0
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                labels_list.append(labels)
+                test = Variable(images.view(100, 1, 28, 28))
+                outputs = model(test)
+                predictions = torch.max(outputs, 1)[1].to(device)
+                predictions_list.append(predictions)
+                correct += (predictions == labels).sum()
+                total += len(labels)
+    
+            accuracy = correct * 100 / total
+            loss_list.append(loss.data)
+            iteration_list.append(count)
+            accuracy_list.append(accuracy)
+            
+        if not (count % 500):
+            print(f'Iteration: {count}, Loss: {loss.data}, Accuracy: {accuracy}')
+print(count)
