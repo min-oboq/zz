ef203b5dbf824f7a6643e6ea54487d3ffd8c232b
min-oboq <mj4870@naver.com>
2023-09-11 11:49:55 +0900

    0911

--- python/torch18.py
+++ python/torch18.py
@@ -83,50 +83,56 @@ optimizer = torch.optim.Adam(model.fc.parameters())
 cost = torch.nn.CrossEntropyLoss()
 print(model)
 
-# def train_model(model, dataloaders, criterion, optimizer, device, num_epochs=13, is_train=True):
-#     since = time.time()
-#     acc_history = []
-#     loss_histor = []
-#     best_acc = 0.0
+def train_model(model, dataloaders, criterion, optimizer, device, num_epochs=13, is_train=True):
+    since = time.time()
+    acc_history = []
+    loss_history = []
+    best_acc = 0.0
     
-#     for epoch in range(num_epochs):
-#         print('Epoch{}/{}'.format(epoch, num_epochs-1))
-#         print('-'*10)
+    for epoch in range(num_epochs):
+        print('Epoch{}/{}'.format(epoch, num_epochs-1))
+        print('-'*10)
         
-#         running_loss = 0.0
-#         running_corrects = 0
+        running_loss = 0.0
+        running_corrects = 0
         
-#         for inputs, lables in dataloaders:
-#             inputs = inputs.to(device)
-#             lables = lables.to(device)
+        for inputs, lables in dataloaders:
+            inputs = inputs.to(device)
+            lables = lables.to(device)
             
-#             model.to(device)
-#             optimizer.zero_grad()
-#             outputs = model(inputs)
-#             loss = criterion(outputs,lables)
-#             _, preds = torch.max(outputs, 1)
-#             loss.backward()
-#             optimizer.step()
+            model.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs,lables)
+            _, preds = torch.max(outputs, 1)
+            loss.backward()
+            optimizer.step()
             
-#             running_loss += loss.item() * inputs.size(0)
-#             running_corrects += torch.sum(preds == lables.data)
-#     epoch_loss = running_loss / len(dataloaders,dataset)
-#     epoch_acc = running_corrects.double() / len(dataloaders,dataset)
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == lables.data)
+    epoch_loss = running_loss / len(dataloaders.dataset)
+    epoch_acc = running_corrects.double() / len(dataloaders.dataset)
     
-#     print('Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
+    print('Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
     
-#     if epoch_acc>best_acc:
-#         best_acc = epoch_acc
+    if epoch_acc>best_acc:
+        best_acc = epoch_acc
     
-#     acc_history.append(epoch_acc.item)
+    acc_history.append(epoch_acc.item)
     
-#     loss_histor.append(epoch_loss)
-#     torch.save(model.state_dict(), os.path.join('C:\Users\admin\zz\pythorch\data\catanddog','{0:0=2d}.pth'.format(epoch)))
-    
-#     print
+    loss_history.append(epoch_loss)
+    torch.save(model.state_dict(), os.path.join(r"C:\Users\admin\zz\pythorch\data\catanddog\train",'{0:0=2d}.pth'.format(epoch)))
+    print()
 
-# time_elapsed = time.time() - since
-# print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-# print('Best Acc: {:4f}'.format
-# (best_acc))
-# return acc_history, loss_history
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best Acc: {:4f}'.format(best_acc))
+    return acc_history, loss_history
+
+params_to_update = []
+for name, param in resnet18.named_parameters():
+    if param.requires_grad == True:
+        params_to_update.append(param)
+        print("\t", name)
+        
+optimizer = optim.Adam(params_to_update)
